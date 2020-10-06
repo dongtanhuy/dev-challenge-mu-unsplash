4a424d6f2f6764002eade996ae398c9d9e95b4f0
Huy Dong <huydt@dgroup.co>
2020-10-06 09:37:38 +0700

    handle hover image

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,13 +4,17 @@ import './Card.scss';
 
 interface Props {
   src: string,
+  label: string,
+  onDelete: () => void
 }
 
-const Card: React.FC<Props> = ({src}: Props) => {
+const Card: React.FC<Props> = ({ src, label, onDelete }: Props) => {
 
   return (
     <div className="Card">
+      <div className="Card__DeleteBtn" onClick={onDelete}>delete</div>
       <img className="Card__Image" width={385} src={src} alt="Card" />
+      <span className="Card__Label">{label}</span>
     </div>
   )
 }
